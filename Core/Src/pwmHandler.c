6212142d60aefd5840a7874627fcab3253b6fe5e
ordu1453 <1muzaffer.4ali.5ordu3@gmail.com>
2025-10-17 17:02:43 +0300

    small fixes in pwm handler

--- Core/Src/pwmHandler.c
+++ Core/Src/pwmHandler.c
@@ -10,6 +10,8 @@
 #include "pwm.h"
 #include "diag.h"
 
+extern SystemState_t currentState;
+
 
 
 void pwmHandlerProcess(uint8_t hasError, SystemState_t state)
@@ -17,9 +19,16 @@ void pwmHandlerProcess(uint8_t hasError, SystemState_t state)
 if(hasError)
 {
 pwmDisable();
+currentState = STATE_INIT;
 }
-else if (state == STATE_CHARGE || state == STATE_DISCHARGE)
+else if (state == STATE_CHARGE)
 {
 pwmEnable();
+currentState = STATE_CHARGE;
+}
+else if (state == STATE_DISCHARGE)
+{
+pwmEnable();
+currentState = STATE_DISCHARGE;
 }
 }
